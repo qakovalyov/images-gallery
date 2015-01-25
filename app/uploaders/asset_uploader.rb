0be0f466076455f64510679b62f628f7ab604fa0
Rudolf Kovalyov <qa.kovalyov@gmail.com>
2015-01-26 01:03:21 +0200

    remove s3 config

--- app/uploaders/asset_uploader.rb
+++ app/uploaders/asset_uploader.rb
@@ -1,7 +1,8 @@
 class AssetUploader < CarrierWave::Uploader::Base
 
-  storage :file unless Rails.env.production?
-  storage :fog if Rails.env.production?
+  storage :file
+  # unless Rails.env.production?
+  # storage :fog if Rails.env.production?
 
   def store_dir
     "assets/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
